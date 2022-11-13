d0e2687d8fdeda477cdc29a3657bae1a8ef4ee71
hamzavig <hamzavig@students.zhaw.ch>
2022-11-13 20:18:30 +0100

    - Updated Institution Class - Added Portfolio data - Updated DESCRIPTION with dependencies

---
+++ R/Institution.R
@@ -0,0 +1,64 @@
+#*******************************************************************************
+# ZHAW
+# package: PA_FEMS
+# Date: 08.11.2022
+# Autor: Vigan Hamzai (hamzavig@students.zhaw.ch)
+#*******************************************************************************
+
+##############################################################
+#' 
+#' Class that contains the whole model of an enterprise or institution.
+#' 
+#' The class inherits from class \code{Node} in package \code{data.tree}
+#' It contains a hierarchical model structure.
+#' 
+#' @import data.tree R6
+#' @export
+#' @rdname Institution
+Institution <- R6Class("Institution",
+                       inherit = Node)
+
+#' @export
+#' @rdname createInstitution
+createInstitution <- function(name, ...) {
+  
+  institution <- Node$new(name)
+  # Create top level nodes "Assets", "Liabilities" and "Operations"
+  institution$AddChild("Assets")
+  institution$AddChild("Liabilities")
+  institution$AddChild("Operations")
+  
+  # Create underlying nodes for "Assets"
+  institution$Assets$AddChild("ShortTerm")
+  institution$Assets$AddChild("LongTerm")
+  institution$Assets$AddChild("FixedAssets")
+  
+  institution$Assets$ShortTerm$AddChild("LiquidAssets")
+  institution$Assets$LongTerm$AddChild("Loans")
+  institution$Assets$LongTerm$AddChild("Mortgages")
+  
+  # Create underlying nodes for "Liabilities"
+  institution$Liabilities$AddChild("ShortTerm")
+  institution$Liabilities$AddChild("LongTerm")
+  institution$Liabilities$AddChild("Equity")
+  
+  institution$Liabilities$ShortTerm$AddChild("Deposits")
+  institution$Liabilities$LongTerm$AddChild("Loans")
+  
+  # Create underlying nodes for "Operations"
+  institution$Operations$AddChild("Revenues")
+  institution$Operations$AddChild("Expenses")
+  
+  institution$Operations$Revenues$AddChild("Interests")
+  institution$Operations$Revenues$AddChild("Commisions")
+  institution$Operations$Revenues$AddChild("Rent")
+  institution$Operations$Revenues$AddChild("Other")
+  
+  institution$Operations$Expenses$AddChild("Interests")
+  institution$Operations$Expenses$AddChild("Salaries")
+  institution$Operations$Expenses$AddChild("Rent")
+  institution$Operations$Expenses$AddChild("Other")
+  
+  return(institution)
+}
+

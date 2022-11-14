0761f04e28a36e1bb2504bd75418cd2dd006225e
hamzavig <hamzavig@students.zhaw.ch>
2022-11-14 13:47:57 +0100

    update doc

--- demo/ToyBank.R
+++ demo/ToyBank.R
@@ -1,11 +1,17 @@
 devtools::install_github("hamzavig/PA_FEMS")
 library(PAFEMS)
-library(FEMS)
-library(FEMSdevPkg)
 
 bank <- createInstitution("Bank")
 bank
 
-library(utils)
-ann_ptf <- "src/data/bankA/ann_ptf.csv"
-ptf <- samplePortfolio(ann_ptf)
+annPortfolio <- "src/data/bankA/ann_ptf.csv"
+pamPortfolio <- "src/data/bankA/pam_ptf.csv"
+
+ann_ptf <- samplePortfolio(annPortfolio)
+ann_ptf$contracts
+
+pam_ptf <- samplePortfolio(pamPortfolio)
+pam_ptf$contracts
+
+serverURL <- "https://demo.actusfrf.org:8080/"
+rfx <- sampleReferenceIndex_YC_EA_AAA()

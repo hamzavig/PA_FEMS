e4feb21449be97dc9b8fd8d96776b92b3e544e1a
hamzavig <hamzavig@students.zhaw.ch>
2022-11-19 21:50:22 +0100

    adjust error log

--- R/EventSeries.R
+++ R/EventSeries.R
@@ -61,7 +61,7 @@ setMethod(f = "EventSeries", signature = c("ContractType", "list", "character"),
 
             response_content <- content(response_events)
             if (response_events$status_code != 200) {
-              print(respone_events)
+              print(response_events)
               print(response_events$status_code)
               print(response_content$error)
               stop("ErrorIn::ContractType::API response error; Check if all necessary contractTerms were set correctly!!!")
